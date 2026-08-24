7ea2d75ca88c539998fb8b448a56499bf4cfdb3a
bricktop-productions <a.bricktop.production@outlook.com>
2026-08-24 12:07:00 -0400

    Simplify ESLint config: avoid @remotion/eslint-config ES module patch issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,41 @@
-import remotion from "@remotion/eslint-config";
-
 export default [
-  ...remotion,
   {
     ignores: ["out/", "public/bricktop-audio.wav", "node_modules/", ".git/", "dist/"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: "readonly",
+        console: "readonly",
+        document: "readonly",
+        window: "readonly",
+        HTMLElement: "readonly",
+        HTMLCanvasElement: "readonly",
+        Audio: "readonly",
+        Image: "readonly",
+        fetch: "readonly",
+        Request: "readonly",
+        Response: "readonly",
+        Headers: "readonly",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "off",
+    },
+    settings: {
+      react: {
+        version: "18.3",
+      },
+    },
   },
 ];
